16ab750b2b295e12ceb00d77a988b7364562cdfb
sirparadox <sirparadox@aac93f5c-2ba8-475e-ac9c-ab603b730876>
2011-05-13 21:37:14 +0000

    tweeks to wormholes

--- GameCode/DB/BP01-fix.sql
+++ GameCode/DB/BP01-fix.sql
@@ -738,5 +738,4 @@ SET IDENTITY_INSERT tblPlayer OFF
 UPDATE tblSolarSystem SET OwnerID=0
 UPDATE tblSolarSystem SET SystemName = SystemName + '(S)', SystemType=0 WHERE SystemID IN (97, 95, 89, 91)
 UPDATE tblSolarSystem SET SystemType=2 WHERE SystemID IN (98, 96, 90, 92, 75, 76, 77, 78, 82, 83, 84, 85)
-UPDATE tblWormhole SET WormholeFlag=13 WHERE System1ID IN (97, 95, 89, 91) OR System2ID IN (97, 95, 89, 91)
-
+UPDATE tblWormhole SET WormholeFlag=13 
